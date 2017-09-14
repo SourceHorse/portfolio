4b0c5266fb1515eb68d27ff99de303ec42a0909e
Ross Scarborough <rossscarborough@gmail.com>
2017-09-14 15:38:50 -0400

    Rough Layout Commit

--- js/index.js
+++ js/index.js
@@ -1,8 +1,23 @@
 function dropMenu() {
   $('#navbar').toggleClass('drop-menu');
 }
+
 $('a').click(function() {
   if ($('#navbar').hasClass('drop-menu')) {
     $('#navbar').removeClass('drop-menu');
   }
+});
+
+function scrollTo(tgt) {
+  $('html,body').animate({
+    scrollTop: $(tgt).offset().top
+  }, 1000);
+};
+
+$(window).scroll(function() {
+	if (($(window).scrollTop() < $('#portfolio').position().top) || ($(window).scrollTop() >= $('#contact').position().top)) {
+    $('#navbar').children().css('color', 'white');
+  } else {
+    $('#navbar').children().css('color', 'black');
+  };
 });
